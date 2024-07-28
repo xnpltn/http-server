e2f22ed52a7ff7d24cf6a8b594148066b9730493
Platon <xnpltn@gmail.com>
2024-07-28 18:49:13 +0200

    final

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xnpltn/web
+
+go 1.22.4
--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	listener, err := net.Listen("tcp", ":8080")
+	port := flag.Uint64("port", 6969, "http port")
+	flag.Parse()
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,10 +30,29 @@ func main() {
 func handleConn(c net.Conn) {
 	defer c.Close()
 
-	data, err := os.ReadFile("index.html")
+	buffer := make([]byte, 512)
+	_, err := c.Read(buffer)
+	if err != nil {
+		log.Println(err)
+	}
+
+	var file string
+	requestData := strings.Split(string(buffer), "\r\n")
+
+	// for better experience, create a Parser to parse the request (method, path, relative path, headers, etc)
+	if requestData[0] == "GET / HTTP/1.1" {
+		file = "index.html"
+	} else {
+		file = "404.html"
+	}
+
+	data, err := os.ReadFile(file)
 	if err != nil {
 		log.Println(err)
 		return
+	}
+	if len(data) <= 1 {
+		data = []byte("something went wronG")
 	}
 
 	response := fmt.Sprintf(
